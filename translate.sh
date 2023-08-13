a497fa9acf9462ab9650dfc5b00486ae23fa1faf
ancestral <ancestral@users.noreply.github.com>
2023-08-12 23:49:59 -0500

    Update translate.sh
    Fixed issue with line breaks

--- translate.sh
+++ translate.sh
@@ -47,33 +47,46 @@ echo "**************************************************************************
 echo ""
 echo "Preparing files..."
 
-exit
-
 rm -f matches.xml
 rm -f Murica.xml
+cp English.xml English_0.xml
 
+# Preprocessing
 echo '<?xml version="1.0" encoding="utf-8"?>' >> matches.xml
 echo '<contentList>' >> matches.xml
 
-# Preprocessing
+# Force each element onto one line by folding newlines
+# (Larian uses CRLF)
+perl -pi -e 's/([^>])\r\n$/$1\\n /g while m/[^>]\r\n$/' English_0.xml
+
 echo "Finding lines to replace..."
 
+# Read each line from the spellings.txt to substitute
+# Take these lines and put them into a matches file
 while IFS= read -r line; do
   FIND=${line% => *}
+	
+	# If the line has a `(hash|hash)` on it, that means only replace for those
+	# contentid’s.
 	if [[ $line =~ "("*")" ]]; then
 		ONLY=\(${line##*(}
     ONLY=$(echo $ONLY | tr -d "()")
-		perl -ne 'print if /'"$ONLY"'.+\b'"$FIND"'\b/i' English.xml >> matches.xml
+		perl -ne 'print if /'"$ONLY"'.+\b'"$FIND"'\b/i' English_0.xml >> matches.xml
 	else	
-		perl -ne 'print if /\b'"$FIND"'\b/i' English.xml >> matches.xml
+		perl -ne 'print if /\b'"$FIND"'\b/i' English_0.xml >> matches.xml
 	fi
 done < spellings.txt
 
+rm English_0.xml
+
 echo '</contentList>' >> matches.xml
+
+# Delete duplicate lines (it could happen if one line contains multiple
+# different word substitutions.)
 perl -ne 'print unless $seen{$_}++;' matches.xml >> Murica.xml
 rm -f matches.xml
 
-# Substitutions
+# Make substitutions
 echo "Making substitutions..."
 
 while IFS= read -r line; do
@@ -88,6 +101,9 @@ done < spellings.txt
 
 echo "Cleaning up..."
 
+# Fix newlines
 perl -pi -e 'tr/\r//d' Murica.xml
+perl -pi -e 's/\\n /\n/g' Murica.xml
+perl -pi -e 's/\n/\r\n/g' Murica.xml
 
 echo "Done"
